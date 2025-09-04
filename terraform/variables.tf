ae9f2dffe31ff9ad8b389b5bb4f5123fcbbb9844
JeanLebeau <jeandelebeau@icloud.com>
2025-09-04 07:53:39 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,13 @@ variable "ssh_public_key" {
   type        = string
 }
 
+variable "availability_domain_number" {
+  type        = number
+  description = "The availability domain to use (1, 2, or 3). If you get an 'out of capacity' error, destroy the stack and try a different number."
+  default     = 1
+
+  validation {
+    condition     = contains([1, 2, 3], var.availability_domain_number)
+    error_message = "The Availability Domain number must be 1, 2, or 3."
+  }
+}
